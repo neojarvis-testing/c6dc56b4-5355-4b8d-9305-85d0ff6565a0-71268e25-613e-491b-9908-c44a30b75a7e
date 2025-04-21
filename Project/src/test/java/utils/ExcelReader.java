9f68feaec1806a1dd7f5d21988a0dc41533ed0f8
dharenkrishna <dharenkrishna@gmail.com>
2025-04-21 19:16:18 +0000

    Initial Commit

--- Project/src/test/java/utils/ExcelReader.java
+++ Project/src/test/java/utils/ExcelReader.java
@@ -1,5 +1,26 @@
 package utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelReader {
-    
+
+    public static String readdata(String filepath, String sheetname, int rownumber, int colnumber) {
+        try (FileInputStream file = new FileInputStream(filepath);
+        XSSFWorkbook workbook = new XSSFWorkbook(file)) {
+            XSSFSheet sheet = workbook.getSheet(sheetname);
+            XSSFRow row = sheet.getRow(rownumber);
+            XSSFCell col = row.getCell(colnumber);
+            DataFormatter formatter = new DataFormatter();
+            return formatter.formatCellValue(col);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
 }
